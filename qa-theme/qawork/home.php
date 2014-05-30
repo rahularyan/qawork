8bce8053185adfcb1c21c7c61f6a2c2176d08585
rahularyan <rah12@live.com>
2014-05-30 17:09:51 +0530

    Added widget position

--- qa-theme/qawork/home.php
+++ qa-theme/qawork/home.php
@@ -14,20 +14,18 @@
 						<?php $this->cs_position('Home Count'); ?>
 					</div>
 					<div class="col-md-4">
-						<a href="#" class="btn register">Register</a>
-						<?php 
-							if (!!qa_opt("facebook_app_id")) {
-								  /* if (!isset($fb_root_initiallized) && !$fb_root_initiallized) {
-										$themeobject->output('<div id="fb-root"></div>');
-										$fb_root_initiallized = true ;
-								  } */
-								  $on_click_event = cs_generate_facebook_invite_script(qa_opt("facebook_app_id"), array('url' => qa_opt("site_url")))  ;
-								  $button = '<button class="btn btn-block btn-facebook" onclick="'.$on_click_event.'">'.qa_lang_html('cs_social_posting/send_facebook_invite').'</button>' ;
-								  $themeobject->output($button );
-							}else {
-								  $themeobject->output("Please provide Facebook application Id to enable this option in Theme Options -> Social Login ");
-							}
-						?>
+						<div class="big-btns">
+							<a href="#" class="btn register">Register</a>
+							<?php 
+								if (!!qa_opt("facebook_app_id")) {
+									$on_click_event = cs_generate_facebook_invite_script(qa_opt("facebook_app_id"), array('url' => qa_opt("site_url")))  ;
+									$button = '<button class="btn btn-facebook" onclick="'.$on_click_event.'">'.qa_lang_html('cs_social_posting/invite_friends').'</button>' ;
+									$this->output($button );
+								}else {
+									$this->output("Please provide Facebook application Id to enable this option in Theme Options -> Social Login ");
+								}
+							?>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -35,6 +33,28 @@
 		<div class="home-featured">			
 			<div class="container">
 				<?php $this->cs_position('Home Featured'); ?>
+			</div>
+		</div>
+		<div class="home-happening">			
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<?php $this->cs_position('Home Activity'); ?>
+					</div>
+					<div class="col-md-8">
+						<div class="row">
+							<div class="col-md-12">
+								<?php $this->cs_position('Home Users'); ?>
+							</div>
+							<div class="col-md-6">
+								<?php $this->cs_position('Home Questions'); ?>
+							</div>
+							<div class="col-md-6">
+								<?php $this->cs_position('Home Answers'); ?>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	<?php
