510a97bf447a52c75a205732457d1fe979953e5a
rahularyan <rah12@live.com>
2014-06-18 09:14:30 +0530

    improved upload and some other fixes

--- qa-plugin/qawork/qa-plugin.php
+++ qa-plugin/qawork/qa-plugin.php
@@ -18,8 +18,8 @@ if (!defined('QA_VERSION')) { // don't allow this page to be requested directly 
 	exit;
 }
 
-//	Ensure no PHP errors are shown in the Ajax response
-	@ini_set('display_errors', 0);
+//	Ensure no PHP errors are shown
+@ini_set('display_errors', 0);
 
 //return; // use this if theme is disabled
 
