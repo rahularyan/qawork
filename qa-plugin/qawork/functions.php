9c54e97549c53909070c43d97c11b035f6a971e2
rahularyan <rah12@live.com>
2014-06-18 09:14:30 +0530

    improved upload and some other fixes

--- qa-plugin/qawork/functions.php
+++ qa-plugin/qawork/functions.php
@@ -1217,3 +1217,180 @@ function qw_check_pref_for_event($userid , $event , $all_preferences='' )
 function qw_get_all_post_metas($postid){
 	return qa_db_read_all_assoc(qa_db_query_sub('SELECT title, content FROM ^postmetas WHERE postid=#', $postid ), 'title', 'content');
 }
+
+function qw_resize_image($source_image, $destination_filename, $width = 200, $height = 150, $quality = 100, $crop = true, $resource = false)
+{
+
+	if( ! $image_data = getimagesize( $source_image ) )
+	{
+		return false;
+	}
+
+	switch( $image_data['mime'] )
+	{
+		case 'image/gif':
+			$get_func = 'imagecreatefromgif';
+			$suffix = ".gif";
+		break;
+		case 'image/jpeg';
+			$get_func = 'imagecreatefromjpeg';
+			$suffix = ".jpg";
+		break;
+		case 'image/png':
+			$get_func = 'imagecreatefrompng';
+			$suffix = ".png";
+		break;
+	}
+	
+	switch ($image_data['mime']) {
+		case 'image/jpeg':
+		case 'image/jpg':
+			$save_func = 'ImageJPEG';
+			break;
+
+		case 'image/png':
+			$save_func = 'ImagePNG';
+			break;
+
+		case 'image/gif':
+			$save_func = 'ImageGIF';
+			break;
+
+		default:
+			$image_save_func = 'ImageJPEG';
+	}
+
+	$img_original = call_user_func( $get_func, $source_image );
+	$old_width = $image_data[0];
+	$old_height = $image_data[1];
+	$new_width = $width;
+	$new_height = $height;
+	$src_x = 0;
+	$src_y = 0;
+	$current_ratio = round( $old_width / $old_height, 2 );
+	$desired_ratio_after = round( $width / $height, 2 );
+	$desired_ratio_before = round( $height / $width, 2 );
+
+	if( $old_width < $width || $old_height < $height )
+	{
+		/**
+		 * The desired image size is bigger than the original image. 
+		 * Best not to do anything at all really.
+		 */
+		return false;
+	}
+
+
+	/**
+	 * If the crop option is left on, it will take an image and best fit it
+	 * so it will always come out the exact specified size.
+	 */
+	if( $crop )
+	{
+		/**
+		 * create empty image of the specified size
+		 */
+		$new_image = imagecreatetruecolor( $width, $height );
+
+		/**
+		 * Landscape Image
+		 */
+		if( $current_ratio > $desired_ratio_after )
+		{
+			$new_width = $old_width * $height / $old_height;
+		}
+
+		/**
+		 * Nearly square ratio image.
+		 */
+		if( $current_ratio > $desired_ratio_before && $current_ratio < $desired_ratio_after )
+		{
+			if( $old_width > $old_height )
+			{
+				$new_height = max( $width, $height );
+				$new_width = $old_width * $new_height / $old_height;
+			}
+			else
+			{
+				$new_height = $old_height * $width / $old_width;
+			}
+		}
+
+		/**
+		 * Portrait sized image
+		 */
+		if( $current_ratio < $desired_ratio_before  )
+		{
+			$new_height = $old_height * $width / $old_width;
+		}
+
+		/**
+		 * Find out the ratio of the original photo to it's new, thumbnail-based size
+		 * for both the width and the height. It's used to find out where to crop.
+		 */
+		$width_ratio = $old_width / $new_width;
+		$height_ratio = $old_height / $new_height;
+
+		/**
+		 * Calculate where to crop based on the center of the image
+		 */
+		$src_x = floor( ( ( $new_width - $width ) / 2 ) * $width_ratio );
+		$src_y = round( ( ( $new_height - $height ) / 2 ) * $height_ratio );
+	}else{
+		if( $old_width > $old_height )
+		{
+			$ratio = max( $old_width, $old_height ) / max( $width, $height );
+		}else{
+			$ratio = max( $old_width, $old_height ) / min( $width, $height );
+		}
+
+		$new_width = $old_width / $ratio;
+		$new_height = $old_height / $ratio;
+
+		$new_image = imagecreatetruecolor( $new_width, $new_height );
+	}
+	
+	if($save_func == 'ImagePNG'){
+		//http://www.akemapa.com/2008/07/10/php-gd-resize-transparent-image-png-gif/
+		imagealphablending($new_image, false);
+		imagesavealpha($new_image, true);
+		$transparent = imagecolorallocatealpha($new_image, 255, 255, 255, 127);
+		imagefilledrectangle($new_image, 0, 0, $new_width, $new_height, $transparent);
+	}
+
+	imagecopyresampled( $new_image, $img_original, 0, 0, $src_x, $src_y, $new_width, $new_height, $old_width, $old_height );
+
+	if($resource){
+		return $new_image;
+	}else{
+		/**
+		 * Save it as a JPG File with our $destination_filename param.
+		 */
+		if($save_func == "imageJPG" || $save_func == "ImageJPEG"){
+			if(!$save_func($new_image, $destination_filename, $quality)){
+				throw new Exception("Cannot save file ".$destination_filename);
+			}
+		}else{
+			if(!$save_func($new_image, $destination_filename)){
+				throw new Exception("Cannot save file ".$destination_filename);
+			}
+		}
+
+		imagedestroy( $new_image );
+		imagedestroy( $img_original );
+
+	}
+	return true;
+}
+
+function qw_is_image($path)
+{
+	$a = getimagesize($path);
+	$image_type = $a[2];
+	
+	if(in_array($image_type , array(IMAGETYPE_GIF , IMAGETYPE_JPEG ,IMAGETYPE_PNG , IMAGETYPE_BMP)))
+	{
+		return true;
+	}
+	return false;
+}
