0cc0334853e5365249141212314a69ceb9d2b82c
Amiya sahu <amiya053@gmail.com>
2014-06-24 08:58:34 +0530

    Updated the selecting mechanism

--- qa-plugin/qawork/addons/IM/script.js
+++ qa-plugin/qawork/addons/IM/script.js
@@ -115,6 +115,16 @@ $(document).ready(function() {
 			init : function () {
 				Messages.readAllUsers();
 				// if a user is selected previously mark it as selected after the reload also 
+				  	if (!!Messages.selectedUserDetails) {
+							$.each($('#msg-user-list > ul > li'), function(index, val) {
+								$val = $(val) ;
+								if ($val.data('mid') == Messages.selectedUserDetails.userid) {
+									$val.siblings('li').removeClass('selected')
+									      .end().addClass('selected') ;
+								};
+							});
+					};
+				}, 500);
 				Messages.readAllMessages();
 				Messages.lastRefreshed = new Date();
 				Messages.getAllUsers();
@@ -175,16 +185,7 @@ $(document).ready(function() {
 		window.setInterval(function(){
 		  	Messages.refreshAll();
 		  	Messages.populateSelectedMessage();	
-		  	if (!!Messages.selectedUserDetails) {
-					$.each($('#msg-user-list > ul > li'), function(index, val) {
-						$val = $(val) ;
-						if ($val.data('mid') == Messages.selectedUserDetails.userid) {
-							$val.siblings('li').removeClass('selected')
-							      .end().addClass('selected') ;
-						};
-					});
-			};
-		}, 500);
+		  	
 
 });
 
